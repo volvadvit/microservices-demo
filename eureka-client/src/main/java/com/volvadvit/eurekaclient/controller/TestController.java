d55868b54990cdd52da6261cc01df0594572ad9b
Vadim Volkov <vadimvolkov.volkov2@burberry.com>
2025-01-26 22:03:59 +0500

    Configure client to test load balancing

--- eureka-client/src/main/java/com/volvadvit/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/volvadvit/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.volvadvit.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     /**
      * To call through Gateway use <a href="http://localhost:8082/main/test">http://localhost:8082/main/test</a>
      */
     @GetMapping("/test")
     public String test() {
-         return "test";
+         return instanceId;
     }
 }
