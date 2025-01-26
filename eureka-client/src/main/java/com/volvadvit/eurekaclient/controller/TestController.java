bd0960633a87a493b06d9205c362bbe1742ab402
Vadim Volkov <vadimvolkov.volkov2@burberry.com>
2025-01-26 21:49:13 +0500

    Configure route for gateway

--- eureka-client/src/main/java/com/volvadvit/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/volvadvit/eurekaclient/controller/TestController.java
@@ -1,11 +1,16 @@
 package com.volvadvit.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
 
+    /**
+     * To call through Gateway use <a href="http://localhost:8082/main/test">http://localhost:8082/main/test</a>
+     */
     @GetMapping("/test")
     public String test() {
          return "test";
