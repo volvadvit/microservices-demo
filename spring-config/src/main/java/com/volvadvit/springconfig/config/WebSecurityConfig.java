c6a5820fa841ac4a3d85cb9994e1bd3856c7ae9c
Vadim Volkov <volvadvit@gmail.com>
2025-01-31 01:18:03 +0500

    Add Config server

---
+++ spring-config/src/main/java/com/volvadvit/springconfig/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.volvadvit.springconfig.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
